8587b996a76a2ad69814b1ecfa124bc02265fb72
maintainker <56953458+maintainker@users.noreply.github.com>
2020-01-02 13:12:25 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 function about_tab(int){//about tab
     var classname=document.getElementsByClassName("id_card");
     var tab_name=document.getElementsByClassName("tap_menu");
@@ -17,37 +18,6 @@ function about_tab(int){//about tab
         document.getElementsByClassName("html")[0].classList.remove("active");
         document.getElementsByClassName("css")[0].classList.remove("active");
         document.getElementsByClassName("javascript")[0].classList.remove("active");
-        document.getElementsByClassName("react")[0].classList.remove("active");
-        document.getElementsByClassName("algo")[0].classList.remove("active");
-    }
-}
-function windowChange(){
-    var top=window.innerHeight;
-    window.scrollY = top;
-    document.documentElement.scrollTop = top;
-}
-document.addEventListener("scroll",nav_chage);// nav resize
-function nav_chage(){
-    var nav = window.scrollY || document.documentElement.scrollTop;
-    var Cl =document.getElementsByClassName("nav");
-    var charic = document.getElementsByClassName("charic");
-    if(nav > 0){
-        Cl[0].classList.add("on");
-        charic[0].classList.add("on");
-    }else{
-        Cl[0].classList.remove("on");
-        charic[0].classList.remove("on");
-    }
-}
-function main_resize(){// main page resize
-    var win_ratio = window.innerHeight/window.innerWidth;
-    var main = document.getElementsByClassName("main_back");
-    if(win_ratio>1066/1600){
-        main[0].classList.add("img_long");
-    }else{
-        main[0].classList.remove("img_long");
-    }
-}tElementsByClassName("javascript")[0].classList.remove("active");
         document.getElementsByClassName("react")[0].classList.remove("active");
         document.getElementsByClassName("algo")[0].classList.remove("active");
     }
